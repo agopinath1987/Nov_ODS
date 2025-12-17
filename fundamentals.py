772fedd62f2eebd20975001b0deadd05096fa342
agopinath1987 <agopinath1987@gmail.com>
2025-12-17 18:29:57 +0530

    ODS python project nov release
    ODS python project nov release

---
+++ fundamentals.py
@@ -0,0 +1,327 @@
+# print --> it is used to display information on the screeen
+
+#syntax
+# print(exp)
+
+# print(10+10)
+# print()
+# print("welcome to pythonlife")
+
+#below code is used for addition operation
+
+
+
+# num_1 = 10
+# num_2 = 10
+# result = num_1 + num_2
+# print(result)
+
+
+
+
+'''
+above code is used to perform addition operation
+num_1 is a variable it contains a value 10
+num_2 is a variable it contains a value 10
+later, in next line performed addition operation by using + operator
+result has been displayed by using print statement
+'''
+
+
+"""
+1.
+2.3.
+multi line comments
+"""
+
+#syntax for variable creation
+#variable =  value
+# age = 35
+# print(id(age))
+
+
+# product_cost = 1000
+# print(id(product_cost))
+
+
+# _employee_id = 234
+# print(_employee_id)
+
+
+# @st_person_number = 456789
+# print(@st_person_number) #invalid
+
+
+# employee_1_salary = 250000
+# print(employee_1_salary)
+
+# name = "arjun"
+# Name = "kiran"
+# NAME = "ayesha"
+
+# print(id(name))
+# print(id(Name))
+# print(id(NAME))
+
+
+# IF = "pythonlife"
+# print(IF)
+
+
+# 1.Camel Case
+# studentName = "praveen"
+# print(studentName)
+
+# # 2.snake_case
+# student_name = "manju"
+# print(student_name)
+
+
+# # 3.PascalCase
+# StudentName = "muni"
+# print(StudentName)
+
+
+
+# product_cost = -58465498
+# print(product_cost)
+# print(type(product_cost))
+
+
+# height = -5.7489
+# print(height)
+# print(type(height))
+
+
+
+# string --> it is used to represent sequence of characters , and it is used to store textual information
+# sentence = 'welcome to pythonlife 7 O clock'
+# print(sentence)
+# print(type(sentence))
+# sentence_2 = "welcome to pythonlife 7 O'clock"
+# print(sentence_2)
+# print(type(sentence_2))
+# sentence_3 = """welcome to pythonlife 7 _ pm
+# 1.sample data
+# 2.pythonlife
+# 3.7pm
+
+# he said ,"he will attend by 7pm"
+
+
+
+
+#  """
+# print(sentence_3)
+# print(type(sentence_3))
+
+
+
+# type conversion --> implicit(automatic) type conversion 2.explicit type conversion (manual)
+
+
+#int ---> float
+# product_cost = 1000
+# print(product_cost)
+# print(type(product_cost))
+
+
+# product_cost = 1000
+# float_conversion = float(product_cost)
+# print(float_conversion)
+# print(type(float_conversion))
+
+#float --> int
+# kilometers = 1.25
+# int_conversion = int(kilometers)
+# print(int_conversion)
+# print(type(int_conversion))
+
+
+#str --> int
+# employee_salary = "25000050"
+# print(employee_salary)
+# print(type(employee_salary))
+# int_conversion = int(employee_salary)
+# print(type(int_conversion))
+
+
+# str --> float
+# height = "5.7"
+# print(type(height))
+# float_conversion = float(height)
+# print(float_conversion)
+# print(type(float_conversion))
+
+
+# num_1 = int(input("enter number 1: "))
+# num_2 = int(input("enter number 2: "))
+# result = num_1 + num_2
+# print(result)
+
+
+# first_name = "250"
+# last_name = "10"
+# print(first_name + last_name)
+
+
+# num_1 = 25
+# num_2 = 25.50
+# print(num_1+num_2)
+
+
+# school_name = "aditya"
+
+# vasu = 12345
+
+# a = 25000
+
+
+# mail_id = "vasukumarpalani@gmail.com"
+
+
+# print("python"+"life")
+
+
+# string --> 
+
+
+##################### NOV 10 2025 #######
+
+#LIST
+
+# - A list is a mutable, ordered sequence of elements.
+# - Elements can be of different data types.
+# - Lists are defined using square brackets `[]`.
+
+# sample_list = [25,5.7,"welcome to pythonlife",(35,15.7,"sample"),[1,2,3],{2,45}]
+# print(sample_list)
+# print(id(sample_list))
+# print(type(sample_list))
+# sample_list.append("pythonlife")
+# print(sample_list)
+# print(id(sample_list))
+
+
+#tuples
+# voter_id = (35,15.75,"pythonlife",[1,2,3,5.7],(5.7,25,35,"python"),{1,23})
+# print(voter_id)
+# print(type(voter_id))
+# voter_id.append("sample")#invalid
+# print(voter_id)
+
+#set --> mutable data type
+# sample_set = {35,415,156.758,"charan","kiran","prasad","charan",25,(1,2,34),{1,2,3}}
+# print(sample_set)
+# print(type(sample_set))
+
+
+#dictionary
+# sample_dict = {"user1":"user@123","user2":"user2@123","user3":"user3@123",}
+# print(sample_dict)
+# print(type(sample_dict))
+
+
+# user_data = {
+#     1:"vasu",
+#     2:"ravi",
+#     3:"kiran",
+#     4:"kumar",
+#     5:"ayesha",
+#     # 1:"raj kumar"
+#     (1,3):"data",
+#     "vasu":123456,
+#     # [2,3,4]:"sample",
+#     # {1:123}:"sample dictionary",
+#     6:[1,2,4],
+#     7:{1,2,3},
+#     8:{1:23,},
+#     9:5.7,
+#     10:"kiran",
+#     11:1,
+# }
+# print(user_data)
+
+
+
+# boolean
+# sample = True
+# print(sample)
+# print(type(sample))
+
+# sample2 = False
+# print(sample2)
+# print(type(sample2))
+
+# print(bool(1))
+# print(bool(0))
+
+# complex data type
+# In Python, a complex data type is used to represent complex numbers. A complex number consists of a real part and an imaginary part, and it’s written in the form a + bj
+
+# num_1 = 5+10j
+# print(num_1)
+# print(type(num_1))
+# num_2 = 5+5j
+# print(num_2)
+# print(type(num_2))
+
+# print(num_1+num_2)
+
+
+# sequence_type--> list,tuple,sets,strings,dict
+
+
+
+# voter_data = ["srinu","srinivas","vasu","kumar","ayesha","basheer","raju","srinu","vasu"]
+# print(voter_data)
+# sample = set(voter_data)
+# print(sample)
+# sample_2 = list(sample)
+# print(sample_2)
+
+
+# pan_card = ("cxcp457",2548,49879)
+
+# product_data = {
+#     "rice":10,
+#     "oil":120,
+#     "sugar":20,
+#     "oil":130,
+# }
+
+
+# voter_id = {25,45,78,85,96,785,985,25,25,25,45}
+# print(voter_id)
+# sample = tuple(voter_id)
+# print(sample)
+
+
+# sample = [25,25,(1,2,34),[1,2.34],]
+# print(sample)
+
+# user_data = {
+#     "user@gmail.com":"vasu@123",
+#     "ayesha@gmail.com":"ayesha@123",
+
+# }
+
+
+# emp_data ={
+#     1:["vasu","frontend",123456789],
+
+# }
+
+
+# Whats the major difference btw sets and tuples apart from bracets?
+
+
+# product_data = {
+#     "rice":10,
+#     "oil":120,
+#     "sugar":20,
+#     "oil":130,
+# }
+
+# sample = tuple(product_data)
+# print(sample)
